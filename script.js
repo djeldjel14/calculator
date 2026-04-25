a879db7cf1032ecc0f6a6a90085b9608c804d83c
djeldjel14 <khaleddjeldjel3@gmail.com>
2026-04-25 10:02:35 +0100

    handle division by zero

--- script.js
+++ script.js
@@ -24,7 +24,14 @@ function calculate(){
     if(operator === '+') result = num1 + num2;
     else if(operator === '-') result = num1 - num2;
     else if(operator === '*') result = num1 * num2;
-    else if(operator === '/') result = num1 / num2;
+    else if(operator === '/'){ 
+        if(num2 == 0){
+            result = "cannot divide by 0";
+        }
+        else{
+        result = num1 / num2;
+        }
+    }
     display.textContent = result;
     text = '';
 }
